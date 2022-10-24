42b02fcaa0438f19e3b1632949d7cc23fd2b6240
mertdemirtas <m.demirtas99@icloud.com>
2022-10-24 18:31:39 +0300

    deleted unused comment lines

--- ShoppingApp/Screens/Detail Screen/ProductDetailPageViewController.swift
+++ ShoppingApp/Screens/Detail Screen/ProductDetailPageViewController.swift
@@ -39,78 +39,6 @@ class ProductDetailPageViewController: BaseViewController<ProductDetailPageViewM
         super.viewDidLoad()
         viewModel.delegate = self
         viewModel.getProductCount()
-        //        let salo = EntityManagerWorker.init(entityName: "Product", entityOperation: .getObjects)
-        
-        //        coreDataManager.deleteAllData(with: salo, completion: { (result: Result<CoreDataResult, CoreDataErrors>) in
-        //            print(result)
-        //
-        //        })
-        
-        //        let dictionaryData = ["productName": "BCA", "productDescription": "Desc", "productImage": "empty", "productCount": 5, "productPrice": 12.5] as [String : Any]
-        //
-        //        let dictionaryDataa = ["productName": "ABC" , "productDescription": "Desc", "productImage": "empty", "productCount": 32 , "productPrice": 12.5] as [String : Any]
-        //
-        let object = ProductCoreDataModel(productName: "Name", productDescription: "Desc", productPrice: 2, productImage: "img", productCount: 1)
-        let entityObject = EntityObject.init(entityObject: object)
-        //
-//        let worker = EntityWorker.init(entityName: "Product", entityOperation: .getObjects, entityObject: entityObject)
-        //
-        let salo = EntityWorker.init(entityName: "Product", entityOperation: .getObjects)
-        
-        //        coreDataManager.createObject(with: worker, completion: { (result: Result<CoreDataResult, CoreDataErrors>) in
-        //
-        //        })
-        
-//        coreDataManager.updateEntityObject(with: worker, uniqueElementKey: "productPrice", uniqueElementValue: 1.2, completion: {  (result: Result<CoreDataResult, CoreDataErrors>) in
-//
-//        })
-//
-//        coreDataManager.manageEntity(with: salo, completion: { (result: Result<CoreDataResult, CoreDataErrors>) in
-//            switch(result) {
-//
-//            case .success(let result):
-//                print(result.objectArray)
-//            case .failure(let error):
-//                print(error)
-//            }
-//
-//        })
-        //
-        //        coreDataManager.manageEntity(with: worker, completion: { (result: Result<CoreDataResult, CoreDataErrors>) in
-        //            switch(result) {
-        //            case .success(let data):
-        //                let data = data.objectArray
-        //                print(data)
-        //            case .failure(let error):
-        //                print(error)
-        //            }
-        //        })
-        
-        //        print(salo.entityObject, salo.entityName, salo.entityOperation)
-        //        print(worker.entityName, worker.entityOperation, worker.entityObject)
-        
-        //        coreDataManager.createEntity(entityName: "Product", data: dictionaryData)
-        //        coreDataManager.createEntity(entityName: "Product", data: dictionaryDataa)
-        
-        //        do {
-        //            try coreDataManager.updateEntity(entityName: "Product", uniqueDataAttributeName: "productName", uniqueData: "ABC", data: dictionaryData)
-        //
-        //        }
-        //
-        //        catch {
-        //
-        //        }
-        
-        //        coreDataManager.getEntityData(entityName: "Product", completion: { (result: Result<[NSFetchRequestResult]?, Error>) in
-        //            switch(result) {
-        //
-        //            case .success(let data):
-        //                guard let _ = data else { return }
-        //                print(data as Any)
-        //            case .failure(let error):
-        //                print(error)
-        //            }
-        //        })
     }
     
     override func addViewComponents() {
@@ -146,17 +74,5 @@ extension ProductDetailPageViewController: ProductDetailPageViewModelDelegate {
 extension ProductDetailPageViewController: AddToBasketCardViewDelegate {
     func countDidChanged(currentCount: Int) {
         viewModel.countDidChanged(count: currentCount)
-        
-        let salo = EntityWorker.init(entityName: "Product", entityOperation: .getObjects)
-
-        coreDataManager.manageEntity(with: salo, completion: { (result: Result<CoreDataResult, CoreDataErrors>) in
-            switch(result) {
-            case .success(let data):
-                let data = data.objectArray
-                print(data)
-            case .failure(let error):
-                print(error)
-            }
-        })
     }
 }
